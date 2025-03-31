8ee2e551f85fd372c51319547669f1b0f98f2c02
roseiaa <ljrobinson268@gmail.com>
2025-03-31 04:11:25 +0100

    MongoDB/.env setup and user schema created

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("signup router")
+}
+
+export const login = (req, res) => {
+    res.send("login router")
+}
+
+export const logout = (req, res) => {
+    res.send("logout router")
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
 
-app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT || 5001
+
+app.use("/api/auth", authRoutes);
 
 
-app.listen(5001, () => {
-    console.log('Server running on port 5001')
+app.listen(PORT, () => {
+    console.log('Server running on PORT: ' + PORT)
+    connectDB();
 })
